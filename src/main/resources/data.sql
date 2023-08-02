08a8c0137d4cfa763fe4e76c7541dc91cf6c9b88
Txema Lanchazo <txelan84@gmail.com>
2023-08-02 07:42:07 +0200

    First Commit, files base

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (name, role) VALUES('txema', 0);
+INSERT INTO users (name, role) VALUES('Pedro', 1);
+INSERT INTO users (name, role) VALUES('Pablo', 1);
+INSERT INTO users (name, role) VALUES('Roco', 1);
+INSERT INTO users (name, role) VALUES('Pepito', 1);
