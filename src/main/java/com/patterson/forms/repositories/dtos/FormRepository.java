5b9c425222be92c1b824227bdc13e8a747a6a7f3
Txema Lanchazo <txelan84@gmail.com>
2023-08-02 11:00:41 +0200

    Adjust Repository

--- src/main/java/com/patterson/forms/repositories/dtos/FormRepository.java
+++ src/main/java/com/patterson/forms/repositories/dtos/FormRepository.java
@@ -3,5 +3,7 @@ package com.patterson.forms.repositories.dtos;
 import com.patterson.forms.entities.Form;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FormRepository extends JpaRepository<Form,Long> {
 }
