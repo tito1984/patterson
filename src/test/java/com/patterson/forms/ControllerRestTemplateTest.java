ce79a5e36a436aaf9b944b6eff7bd08d42b86185
Txema Lanchazo <txelan84@gmail.com>
2023-08-02 16:34:59 +0200

    Test for controllers

--- src/test/java/com/patterson/forms/ControllerRestTemplateTest.java
+++ src/test/java/com/patterson/forms/ControllerRestTemplateTest.java
@@ -1,13 +1,23 @@
 package com.patterson.forms;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.patterson.forms.dtos.AnswerDto;
+import com.patterson.forms.dtos.FormDto;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -16,21 +26,30 @@ public class ControllerRestTemplateTest {
     @Autowired
     private TestRestTemplate client;
 
-    private ObjectMapper objectMapper;
-
-    @BeforeEach
-    void setUp() {
-        objectMapper = new ObjectMapper();
-    }
-
     @Test
     void testFindAllForms() {
+        ResponseEntity<FormDto[]> response = client.getForEntity("/forms",FormDto[].class);
+        List<FormDto> forms = Arrays.asList(response.getBody());
 
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(MediaType.APPLICATION_JSON, response.getHeaders().getContentType());
+        assertEquals(2, forms.size());
     }
 
     @Test
     void testSaveAnswer() {
+        AnswerDto answerDto = new AnswerDto("Whats your name?", "Pere");
+        Map<String,Long> pathVariable = new HashMap<>();
+        pathVariable.put("id", 1L);
 
+        ResponseEntity<AnswerDto> response = client.postForEntity(
+                "/answers/1", answerDto, AnswerDto.class,pathVariable);
+        AnswerDto answerDtoResponse = response.getBody();
+
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertEquals(MediaType.APPLICATION_JSON, response.getHeaders().getContentType());
+        assertEquals("Whats your name?",answerDtoResponse.getQuestion());
+        assertEquals("Pere",answerDtoResponse.getAnswer());
     }
 
 }
