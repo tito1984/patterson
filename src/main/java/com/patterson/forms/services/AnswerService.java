e6f6f1590ee15f4ec3d2a07955abe3fd1af1df46
Txema Lanchazo <txelan84@gmail.com>
2023-08-02 11:39:34 +0200

    Change name of contract and attributes

--- src/main/java/com/patterson/forms/services/AnswerService.java
+++ src/main/java/com/patterson/forms/services/AnswerService.java
@@ -1,9 +1,8 @@
 package com.patterson.forms.services;
 
-import com.patterson.forms.entities.Form;
-import com.patterson.forms.entities.User;
+import com.patterson.forms.entities.Answer;
 
 public interface AnswerService {
 
-    void answerForm(String code, String answer, User user, Form form);
+    Answer save(Answer answer);
 }
